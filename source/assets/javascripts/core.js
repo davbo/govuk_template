5a8a9876ad851a824ee0c99c97889181aeb6c976
David King <dave@davbo.org>
2016-02-18 20:21:47 +0000

    Move inline script tags to core.js
    Moving the inline scripts will allow for strict CSP (Content Security
    Policy) headers to be put in place without trouble.

--- source/assets/javascripts/core.js
+++ source/assets/javascripts/core.js
@@ -1,6 +1,17 @@
 (function() {
   "use strict"
 
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+      var d=document,
+          c="appendChild",
+          a=d.createElement("style");
+      a[c](d.createTextNode("@-ms-viewport{width:auto!important}"));
+      d.getElementsByTagName("head")[0][c](a);
+  }
+
+  document.body.className = ((document.body.className) ? document.body.className + ' js-enabled' : 'js-enabled');
+
+
   // add cookie message
   if (window.GOVUK && GOVUK.addCookieMessage) {
     GOVUK.addCookieMessage();
